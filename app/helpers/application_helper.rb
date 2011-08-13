443d9ab818a483c5d945920d9c24d0d1d3f15c49
Oluf Nielsen <olufnielsen@gmail.com>
2011-08-13 20:42:50 +0200

    Finished first cut of the User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,9 @@ module ApplicationHelper
     
   end
   
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round") 
+    
+  end
+  
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,11 +13,6 @@ describe UsersController do
       get 'new'
       response.should have_selector("title", :content => "Sign Up")
     end
-    
-    it "should have a signup page at '/signup'" do
-      get '/signup'
-      response.should have_selector('title', :content => "Sign up")
-    end
   end
 
 end
